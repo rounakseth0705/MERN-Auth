4483d5c387d62f08d7cfa2ae2908c62069b3f864
Rounak Seth <sethrounak7@gmail.com>
2025-12-30 04:10:17 +0530

    Create Authentication Routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,8 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// API Endpoints
 app.get("/", (req,res) => {
     res.send("Server is live");
-})
+});
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
