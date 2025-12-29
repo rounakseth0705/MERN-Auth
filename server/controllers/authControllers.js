34a869b21dffa5335fb5a4c039bcd91080e05e34
Rounak Seth <sethrounak7@gmail.com>
2025-12-30 03:47:19 +0530

    Create Logout controller

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -54,3 +54,17 @@ export const login = async (req,res) => {
         return res.json({ success: false, message: error.message });
     }
 }
+
+export const logout = async (req,res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge: 7*24*60*60*1000
+        });
+        return res.json({ success: true, message: "Logged Out" });
+    } catch(error) {
+        return res.json({ success: false, message: error.message });
+    }
+}
