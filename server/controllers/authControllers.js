4fa320118844307cbd5fe1216822da162147b6a5
Rounak Seth <sethrounak7@gmail.com>
2025-12-30 22:38:02 +0530

    Create Send Verification OTP feature

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -77,3 +77,28 @@ export const logout = async (req,res) => {
         return res.json({ success: false, message: error.message });
     }
 }
+
+// Send Verification OTP to User's Email
+export const sendVerifyOtp = async (req,res) => {
+    try {
+        const { userId } = req.body;
+        const user = await userModel.findById(userId);
+        if (user.isAccountVerified) {
+            return res.json({ success: false, message: "Account already verified" });
+        }
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24*60*60*1000;
+        await user.save();
+        const mailOption = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Account Verification OTP",
+            text: `Your OTP is ${otp}. Verify your account using this OTP.`
+        }
+        await transporter.sendMail(mailOption);
+        return res.json({ success: true, message: "Verification email Sent on Email" });
+    } catch(error) {
+        return res.json({ success: false, message: error.message });
+    }
+}
