5d758524bf927cd79b6104593e52afea2c41a98e
Rounak Seth <sethrounak7@gmail.com>
2025-12-30 23:26:30 +0530

    Create routes for send verification otp and verify email

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { login, logout, register } from "../controllers/authControllers.js";
+import { login, logout, register, sendVerifyOtp, verifyEmail } from "../controllers/authControllers.js";
+import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
+authRouter.post("/verify-account", userAuth, verifyEmail);
 
 export default authRouter;
