bdf8ab33d63935c1219aa68acf7871ef99432120
Rounak Seth <sethrounak7@gmail.com>
2025-12-30 22:16:10 +0530

    Create mail sending feature

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import transporter from "../config/nodemailer.js";
 
 export const register = async (req,res) => {
     const { name, email, password } = req.body;
@@ -22,6 +23,14 @@ export const register = async (req,res) => {
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 7*24*60*60*1000
         });
+        // Sending welcome email
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: "Welcome to my App",
+            text: `Welcome to this website. Your account has been created with email id: ${email}`
+        }
+        await transporter.sendMail(mailOptions);
         return res.json({ success: true });
     } catch(error) {
         res.json({ success: false, message: error.message });
