15b765eca9a88c03fcbd2152ed6a1f30a716d0c8
Rounak Seth <sethrounak7@gmail.com>
2026-01-01 22:32:20 +0530

    Create controller to send reset password OTP

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -96,7 +96,7 @@ export const sendVerifyOtp = async (req,res) => {
             to: user.email,
             subject: "Account Verification OTP",
             text: `Your OTP is ${otp}. Verify your account using this OTP.`
-        }
+        };
         await transporter.sendMail(mailOption);
         return res.json({ success: true, message: "Verification email Sent on Email" });
     } catch(error) {
@@ -137,5 +137,33 @@ export const isAuthenticated = async (req,res) => {
         return res.json({ success: true });
     } catch(error) {
         return res.json({ success: false, message: error.message })
+    }
+}
+
+// Send password reset OTP
+export const sendResetOtp = async (req,res) => {
+    const { email } = req.body;
+    if (!email) {
+        return res.json({ success: false, message: "Email is required" });
+    }
+    try {
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+        user.resetOtp = otp;
+        user.resetOtpExpireAt = Date.now() + 15*60*1000;
+        await user.save();
+        const mailOption = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: "Password Reset OTP",
+            text: `Your OTP for resetting your password is ${otp}. Use this OTP to proceed with resetting your password.`
+        };
+        await transporter.sendMail(mailOption);
+        return res.json({ success: true, message: "OTP sent to your email" });
+    } catch(error) {
+        return res.json({ success: false, message: error.message });
     }
 }
