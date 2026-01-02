125019fe25e936ef4f566a356c3a20b588177150
Rounak Seth <sethrounak7@gmail.com>
2026-01-03 02:30:12 +0530

    Create Home page and its components [Navbar & Header]

---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,9 @@
+const EmailVerify = () => {
+    return(
+        <div>
+            <h1>Email verify</h1>
+        </div>
+    )
+}
+
+export default EmailVerify;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header.jsx";
+import Navbar from "../components/Navbar.jsx";
+
+const Home = () => {
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')] bg-cover bg-center">
+            <Navbar />
+            <Header />
+        </div>
+    )
+}
+
+export default Home;
