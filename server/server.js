042e40f8c68d19261734da0b7ecdd02b4e39cae1
Rounak Seth <sethrounak7@gmail.com>
2026-01-02 15:57:13 +0530

    Create controller and route to get user data

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,9 +15,8 @@ app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
 // API Endpoints
-app.get("/", (req,res) => {
-    res.send("Server is live");
-});
+app.get("/", (req,res) => res.send("Server is live"));
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
