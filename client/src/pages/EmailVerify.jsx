e7cb5f5e23e7841260a9dc0212adde0e24238eef
Rounak Seth <sethrounak7@gmail.com>
2026-01-04 04:21:48 +0530

    Create enter and paste otp functionality

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,7 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets.js";
+import { useRef } from "react";
+
 const EmailVerify = () => {
+    const navigate = useNavigate();
+    const inputRefs = useRef([]);
+    const handleInput = (event,index) => {
+        if (event.target.value.length > 0 && index < inputRefs.current.length - 1) {
+            inputRefs.current[index+1].focus();
+        }
+    }
+    const handleKeyDown = (event,index) => {
+        if (event.key === "Backspace" && event.target.value && index > 0) {
+            inputRefs.current[index-1].focus();
+        }
+    }
+    const handlePaste = (event) => {
+        const paste = event.clipboardData.getData("text");
+        const pasteArray = paste.split("");
+        pasteArray.forEach((char,index) => {
+            if (inputRefs.current[index]) {
+                inputRefs.current[index].value = char;
+            }
+        })
+    }
     return(
-        <div>
-            <h1>Email verify</h1>
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
+            <img onClick={() => navigate("/")} src={assets.logo} alt="" className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer" />
+            <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+                <h1 className="text-white text-2xl font-semibold text-center mb-4">Email Verify OTP</h1>
+                <p className="text-center mb-6 text-indigo-300">Enter the 6-digit code sent to your email id.</p>
+                <div className="flex justify-between mb-8" onPaste={handlePaste}>
+                    {Array(6).fill(0).map((_, index) => (
+                        <input type="text" maxLength="1" key={index} required className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md" ref={(event) => inputRefs.current[index] = event} onInput={(event) => handleInput(event,index)} onKeyDown={(event) => handleKeyDown(event,index)} />
+                    ))}
+                </div>
+                <button className="w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full">Verify email</button>
+            </form>
         </div>
     )
 }
